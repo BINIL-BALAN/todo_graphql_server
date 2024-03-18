c82cab422185bde3d0284b35b330edb3890cfeb5
BINIL E B <binilbalan.e.k@gmail.com>
2024-03-18 09:25:49 +0530

    files edited

--- database/services.js
+++ database/services.js
@@ -1,200 +1,237 @@
-const db = require('./db')
-const {getDateTimeId} = require("./utils/utils")
-const addUser = ({user})=>{
-    const {email} = user
-    console.log(email);
-   return db.User.findOne({email}).then((result)=>{
-    console.log(result);
-     if(result){
+const db = require("./db");
+const { getDateTimeId } = require("./utils/utils");
+const fs = require("fs");
+
+const addUser = (_, { user }) => {
+  const { email, file, name } = user;
+
+  //  console.log(file.encoded);
+  return db.User.findOne({ email })
+    .then((result) => {
+      if (result) {
         return {
-            statusCode:400,
-            message:"User already exist"
+          statusCode: 400,
+          message: "User already exist",
+        };
+      } else {
+        let filePath = ""
+        if (file?.encoded) {
+            const base64Image = file.encoded;
+            const binaryImage = Buffer.from(base64Image.split(';base64,').pop(), 'base64');
+            filePath = 'assets/images/' + (name+"."+file.name.split(".")[1]);
+            console.log(filePath);
+            fs.writeFile(filePath, binaryImage, (err) => {
+              if (err) {
+                console.error('Error saving image:', err);
+              } else {
+                console.log('Image saved successfully');
+              }
+            });
         }
-     }else{
         const newUser = new db.User({
-            id:email,
-            ...details.user
-        })
+            id: email,
+           image:filePath,
+            ...user,
+        });
+        console.log(newUser);
         newUser.save()
-     }
-     return {
-        statusCode:200,
-        message:"User added successfully"
-    }
-   }).catch(error=>{
-    return {
-        statusCode:400,
-        message:"Something went wrong"
-    }
-   })
-}
-
-const getUser = ({email})=>{
-    return db.User.findOne({email}).then((result)=>{
-        if(result){
-            const {id,email,name,task} = result
-            return {
-                result :{ statusCode:200,message:"" },
-                details:{id,email,name,task}
-            }
-        }else{
-            return {
-                result :{ statusCode:400,message:"User not found" },
-                details:{}
-            }
-        }
-    }).catch(error=>{
         return {
-            result :{ statusCode:400,message:"Something went wrong" },
-            details:{}
-        }
+          statusCode: 200,
+          message: "User added successfully",
+        };
+      }
     })
-}
+    .catch((error) => {
+      return {
+        statusCode: 400,
+        message: "Something went wrong",
+      };
+    });
+};
 
+const getUser = (_,{ email }) => {
+    console.log(email);
+  return db.User.findOne({ email })
+    .then((result) => {
+      if (result) {
+        const { id, email, name, task,image } = result;
+        return {
+          result: { statusCode: 200, message: "" },
+          details: { id, email, name,image, task },
+        };
+      } else {
+        return {
+          result: { statusCode: 400, message: "User not found" },
+          details: {},
+        };
+      }
+    })
+    .catch((error) => {
+      return {
+        result: { statusCode: 400, message: "Something went wrong" },
+        details: {},
+      };
+    });
+};
 
-
-const operations = async ({params})=>{
-  const {operation,email,data} = params
-  switch(operation){
-    case "post" :
-        return await addTask(email,data)
-    case "delete" :
-      return await deleteTask(email,data)
+const operations = async (_,{ params }) => {
+  const { operation, email, data } = params;
+  console.log(params);
+  switch (operation) {
+    case "post":
+      return await addTask(email, data);
+    case "delete":
+      return await deleteTask(email, data);
     case "update":
-        return await updateTask(email,data)
-      default :
-        return  {
-            statusCode:400,
-            message:"Invalid operation"
-        }
+      return await updateTask(email, data);
+    default:
+      return {
+        statusCode: 400,
+        message: "Invalid operation",
+      };
   }
+};
 
-}
-
-const addTask = async (email,newTask)=>{
-    try {
-        const userDetails = await getUser({email})
-        const {result,details} = userDetails
-        if(result.statusCode == 200){
-               const {task} = details
-               task.push({...newTask,id:getDateTimeId()})
-           try {
-              const postResult = await db.User.findOneAndUpdate({email},{task},{new:true})
-              console.log(postResult);
-              const {id,name,task:newTaskSet} = postResult
-              return {
-                result :{ statusCode:200,message:"Task added successfully" },
-                details:{id,email,name,task:newTaskSet}
-            }
-           } catch (error) {
-            console.log(error);
-            return {
-                result :{ statusCode:500,message:"operation failed" },
-                details:{}
-            } 
-           }
-        }else{
-            return {
-                result :{ statusCode:500,message:"operation failed" },
-                details:{}
-            } 
-        }
-    } catch (error) {
+const addTask = async (email, newTask) => {
+    // console.log(email);
+  try {
+    console.log("before",email);
+    const userDetails = await getUser(null,{ email });
+    console.log("after");
+    const { result, details } = userDetails;
+    if (result.statusCode == 200) {
+      const { task } = details;
+      task.push({ ...newTask, id: getDateTimeId() });
+      console.log(task);
+      try {
+        const postResult = await db.User.findOneAndUpdate(
+          { email },
+          { task },
+          { new: true }
+        );
+        console.log(postResult);
+        const { id, name, task: newTaskSet } = postResult;
         return {
-            result :{ statusCode:400,message:"Something went wrong" },
-            details:{}
-        }
-    }
-  
- }
-
-const deleteTask =async (email,taskToDelete)=>{
-    const {id:taskId} = taskToDelete
-    try {
-        const userDetails = await getUser({email})
-        const {result,details} = userDetails
-        if(result.statusCode == 200){
-               const currentTask = details.task
-               const task = currentTask.filter(item=>item.id != taskId)
-           try {
-              const postResult = await db.User.findOneAndUpdate({email},{task},{new:true})
-              console.log(postResult);
-              const {id,name,task:updatedask} = postResult
-              return {
-                result :{ statusCode:200,message:"Task deleted successfully" },
-                details:{id,email,name,task:updatedask}
-            }
-           } catch (error) {
-            return {
-                result :{ statusCode:500,message:"operation failed 1" },
-                details:{}
-            } 
-           }
-        }else{
-            return {
-                result :{ statusCode:500,message:"operation failed 1" },
-                details:{}
-            } 
-        }
-    } catch (error) {
+          result: { statusCode: 200, message: "Task added successfully" },
+          details: { id, email, name, task: newTaskSet },
+        };
+      } catch (error) {
+        console.log(error);
         return {
-            result :{ statusCode:400,message:"Something went wrong" },
-            details:{}
-        
+          result: { statusCode: 500, message: "operation failed" },
+          details: {},
+        };
+      }
+    } else {
+      return {
+        result: { statusCode: 500, message: "operation failed" },
+        details: {},
+      };
     }
-}
-}
+  } catch (error) {
+    return {
+      result: { statusCode: 400, message: "Something went wrong" },
+      details: {},
+    };
+  }
+};
 
-const updateTask =async (email,taskToUpdate)=>{
-    const {id:taskId,status} = taskToUpdate
-    try {
-        const userDetails = await getUser({email})
-        const {result,details} = userDetails
-        if(result.statusCode == 200){
-               const currentTask = details.task
-               const task = currentTask.map(item=>{
-                  if(item.id == taskId){
-                    return {...taskToUpdate,status:!status}
-                  }
-                  return item
-               })
-           try {
-              const postResult = await db.User.findOneAndUpdate({email},{task},{new:true})
-              console.log(postResult);
-              const {id,name,task:updatedask} = postResult
-              return {
-                result :{ statusCode:200,message:"Task updated successfully" },
-                details:{id,email,name,task:updatedask}
-            }
-           } catch (error) {
-            return {
-                result :{ statusCode:500,message:"operation failed 1" },
-                details:{}
-            } 
-           }
-        }else{
-            return {
-                result :{ statusCode:500,message:"operation failed 1" },
-                details:{}
-            } 
-        }
-    } catch (error) {
+const deleteTask = async (email, taskToDelete) => {
+  const { id: taskId } = taskToDelete;
+  try {
+    const userDetails = await getUser(null,{ email });
+    const { result, details } = userDetails;
+    if (result.statusCode == 200) {
+      const currentTask = details.task;
+      const task = currentTask.filter((item) => item.id != taskId);
+      try {
+        const postResult = await db.User.findOneAndUpdate(
+          { email },
+          { task },
+          { new: true }
+        );
+        console.log(postResult);
+        const { id, name, task: updatedask } = postResult;
         return {
-            result :{ statusCode:400,message:"Something went wrong" },
-            details:{}
-        
+          result: { statusCode: 200, message: "Task deleted successfully" },
+          details: { id, email, name, task: updatedask },
+        };
+      } catch (error) {
+        return {
+          result: { statusCode: 500, message: "operation failed 1" },
+          details: {},
+        };
+      }
+    } else {
+      return {
+        result: { statusCode: 500, message: "operation failed 1" },
+        details: {},
+      };
     }
-}
-}
+  } catch (error) {
+    return {
+      result: { statusCode: 400, message: "Something went wrong" },
+      details: {},
+    };
+  }
+};
 
-const getTask = ({email})=>{
+const updateTask = async (email, taskToUpdate) => {
+  const { id: taskId, status } = taskToUpdate;
+  try {
+    const userDetails = await getUser(null,{ email });
+    const { result, details } = userDetails;
+    if (result.statusCode == 200) {
+      const currentTask = details.task;
+      const task = currentTask.map((item) => {
+        if (item.id == taskId) {
+          return { ...taskToUpdate, status: !status };
+        }
+        return item;
+      });
+      try {
+        const postResult = await db.User.findOneAndUpdate(
+          { email },
+          { task },
+          { new: true }
+        );
+        console.log(postResult);
+        const { id, name, task: updatedask } = postResult;
+        return {
+          result: { statusCode: 200, message: "Task updated successfully" },
+          details: { id, email, name, task: updatedask },
+        };
+      } catch (error) {
+        return {
+          result: { statusCode: 500, message: "operation failed 1" },
+          details: {},
+        };
+      }
+    } else {
+      return {
+        result: { statusCode: 500, message: "operation failed 1" },
+        details: {},
+      };
+    }
+  } catch (error) {
+    return {
+      result: { statusCode: 400, message: "Something went wrong" },
+      details: {},
+    };
+  }
+};
 
-
-   return email
+const getTask = ({ email }) => {
+  return email;
+};
+const getFormData = ({formData})=>{
+    console.log(formData);
+    return "data received"
 }
-module.exports={
-    addUser,
-    getUser,
-    getTask,
-    operations,
-}
+module.exports = {
+  addUser,
+  getUser,
+  getTask,
+  operations,
+  getFormData
+};
--- index.js
+++ index.js
@@ -1,81 +1,159 @@
-const port  = 8000
-const express = require('express')
-const cors = require('cors')
-const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
-const app = express()
-app.use(cors())
-const { addUser,getUser ,getTask,operations} = require('./database/services')
-const schema = buildSchema(`
- 
-type Result {
-  statusCode:Int,
-  message:String
-}
+const port = 8000;
+const express = require("express");
+const cors = require("cors");
+const { ApolloServer, gql } = require("apollo-server-express");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+const {
+  addUser,
+  getUser,
+  getTask,
+  operations,
+  getFormData,
+} = require("./database/services");
+app.use("/assets", express.static("assets"));
+// const schema = buildSchema(`
+//  scalar Upload
+// type Result {
+//   statusCode:Int,
+//   message:String
+// }
+
+//   type Task {
+//     id:String,
+//     task:String,
+//     status:Boolean
+//   }
+
+//   input TaskInput {
+//     id:String,
+//     task:String,
+//     status:Boolean
+//   }
+
+//   input UserInput {
+//     id:String
+//     name:String
+//     email:String
+//     file:Upload
+//     task:[TaskInput]
+//  }
+
+//   type User {
+//     id:String
+//     name:String
+//     email:String
+//     task:[Task]
+//  }
+
+//  type UserDetails {
+//    result : Result
+//    details : User
+//  }
+//  type Query {
+//   getTask(email:String!): String
+// }
+
+//  input TaskParams {
+//     email:String
+//     operation:String
+//     data:TaskInput
+//  }
+
+//   type Mutation {
+//     addUser(user: UserInput!): Result
+//     getUser(email:String): UserDetails
+//     operations(params:TaskParams):UserDetails
+//   }
+// `);
+
+const root = {
+  // getTask,
+  addUser,
+  getUser,
+  operations,
+  getFormData,
+};
+
+//   app.use('/graphql', graphqlHTTP({
+//     schema: schema,
+//     rootValue: root,
+//     graphiql: true, // Set to true if you want to use GraphiQL for testing
+//   }));
+
+const typeDefs = gql`
+  scalar Upload
+  type Result {
+    statusCode: Int
+    message: String
+  }
 
   type Task {
-    id:String,
-    task:String,
-    status:Boolean
+    id: String
+    task: String
+    status: Boolean
   }
 
   input TaskInput {
-    id:String,
-    task:String,
-    status:Boolean
+    id: String
+    task: String
+    status: Boolean
   }
 
   input UserInput {
-    id:String
-    name:String
-    email:String
-    task:[TaskInput]
- }
+    id: String
+    name: String
+    email: String
+    file: Upload
+    task: [TaskInput]
+  }
 
   type User {
-    id:String
-    name:String
-    email:String
-    task:[Task]
- }
+    id: String
+    name: String
+    email: String
+    image: String
+    task: [Task]
+  }
 
- type UserDetails {
-   result : Result
-   details : User
- }
- type Query {
-  getTask(email:String!): String
-}
+  type UserDetails {
+    result: Result
+    details: User
+  }
+  type Query {
+    getTask(email: String!): String
+  }
 
-
- input TaskParams {
-    email:String
-    operation:String
-    data:TaskInput
- }
+  input TaskParams {
+    email: String
+    operation: String
+    data: TaskInput
+  }
 
   type Mutation {
     addUser(user: UserInput!): Result
-    getUser(email:String): UserDetails
-    operations(params:TaskParams):UserDetails
+    getUser(email: String): UserDetails
+    operations(params: TaskParams): UserDetails
+    getFormData(formData:Upload):String
   }
-`);
+`;
+const resolvers = {
+  Mutation: {
+    ...root,
+  },
+};
+const server = new ApolloServer({ typeDefs, resolvers });
+(async () => {
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
+})();
 
-const root = {
-    getTask,
-    addUser,
-    getUser,
-    operations,
-  };
-
-  app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true, // Set to true if you want to use GraphiQL for testing
-  }));
-
-app.listen(port,()=>{
-    console.log(`server started at http://localhost:${port}/graphql`);
-})
-
-
+app.listen(port,'192.168.1.37', () => {
+  console.log(`server started at http://localhost:${port}/graphql`);
+});
